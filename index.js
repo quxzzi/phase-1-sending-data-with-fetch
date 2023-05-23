631a986a811ff48667127e56139b8d9a032f6251
Kevin Dean <kdean1011@gmail.com>
2023-05-23 16:37:06 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += `<p>${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+  
+  
